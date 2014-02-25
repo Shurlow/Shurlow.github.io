e7ad0a1a032be569650784cc253bc9eeb96da856
Shurlow <Scottyhurlow@gmail.com>
2014-02-25 01:37:15 -0600

    big imporvement to design. Still needs more js.

--- fade.js
+++ fade.js
@@ -1,12 +1,9 @@
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
+document.onload=function() {
+
+	var e = document.getElementById("one");
+	e.onClick() = function
+}
+
+var function(){
+	e.setAttribute("color","blue");
 }
